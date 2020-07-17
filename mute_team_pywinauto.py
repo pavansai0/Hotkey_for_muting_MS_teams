00e7c92cfd2e7f519758590d5742cf1b063d0905
pavan sai <48530179+pavansai0@users.noreply.github.com>
2020-07-17 11:44:14 +0530

    Update mute_team_pywinauto.py

--- mute_team_pywinauto.py
+++ mute_team_pywinauto.py
@@ -4,7 +4,11 @@ import pywinauto.keyboard as keyboard
 temp = Application().start(r"C:\Windows\System32\SndVol.exe")
 #print(app.windows())
 app = temp['Volume Mixer - Speakers (Realtek High Definition Audio)']
-app.child_window(title="Mute for Discord",class_name="ToolbarWindow32").click_input()
+mb=app.child_window(title="Mute for Microsoft Teams", class_name="ToolbarWindow32")
+mb.click_input()
 app.type_keys("%{F4}") # Alt-F4
+
+
+
 
 
